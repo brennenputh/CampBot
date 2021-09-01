7bcdde86416d925f0035a8b0e759dd7b29d5f51d
Brennen Puth <brennenputh@gmail.com>
2021-09-01 13:32:03 -0400

    Update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm") version "1.5.30"
     id("com.github.johnrengelman.shadow") version("6.0.0")
 }
 
