572f7363d86b80142f538e4f9b9a7700689342f0
Brennen Puth <brennenputh@gmail.com>
2021-10-08 08:05:19 -0400

    Compare snowflakes instead of straight values

--- src/main/kotlin/io/github/amerebagatelle/campbotkotlin/Permissions.kt
+++ src/main/kotlin/io/github/amerebagatelle/campbotkotlin/Permissions.kt
@@ -1,12 +1,13 @@
 package io.github.amerebagatelle.campbotkotlin
 
 import dev.kord.common.entity.Permission
+import dev.kord.common.entity.Snowflake
 import me.jakejmattson.discordkt.api.dsl.PermissionContext
 import me.jakejmattson.discordkt.api.dsl.PermissionSet
 
 enum class Permissions : PermissionSet {
     BOT_OWNER {
-        override suspend fun hasPermission(context: PermissionContext) = context.user.id.value == 643577385970827266
+        override suspend fun hasPermission(context: PermissionContext) = context.user.id == Snowflake(643577385970827266)
     },
     GUILD_OWNER {
         override suspend fun hasPermission(context: PermissionContext) = context.getMember()?.isOwner() ?: false
