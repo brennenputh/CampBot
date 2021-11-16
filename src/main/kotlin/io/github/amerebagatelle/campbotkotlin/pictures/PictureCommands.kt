3cdde9990589aca0db7b2b1a82791265b902f4f9
Brennen Puth <brennenputh@gmail.com>
2021-11-16 14:17:50 -0500

    Add repeat overload for post

--- src/main/kotlin/io/github/amerebagatelle/campbotkotlin/pictures/PictureCommands.kt
+++ src/main/kotlin/io/github/amerebagatelle/campbotkotlin/pictures/PictureCommands.kt
@@ -3,7 +3,9 @@ package io.github.amerebagatelle.campbotkotlin.utility
 import dev.kord.common.Color
 import dev.kord.core.behavior.channel.createMessage
 import io.github.amerebagatelle.campbotkotlin.pictures.Pictures
+import kotlinx.coroutines.delay
 import me.jakejmattson.discordkt.arguments.AnyArg
+import me.jakejmattson.discordkt.arguments.IntegerArg
 import me.jakejmattson.discordkt.commands.commands
 import java.io.File
 
@@ -49,10 +51,26 @@ fun pictureCommands() = commands("Pictures") {
         }
     }
     globalCommand("post") {
-        description = "Get a file.  Example: &post staff"
+        description = "Get a file.  Append number to the end for posting more than one (limit 20).  Example: &post staff 1"
         execute(AnyArg) {
             channel.createMessage {
                 addFile(Pictures.randomPicture(args.first).toPath())
+            }
+        }
+        execute(AnyArg, IntegerArg) {
+            if (args.second > 20) {
+                respond {
+                    title = "Error"
+                    description = "Number to post greater than 20, please be more reasonable."
+                    color = Color(255, 0, 0)
+                }
+                return@execute
+            }
+            repeat(args.second) {
+                channel.createMessage {
+                    addFile(Pictures.randomPicture(args.first).toPath())
+                }
+                delay(3000)
             }
         }
     }
