e5c2ce09e4c7d8c399367536c36b584f77bb0728
Brennen Puth <brennenputh@gmail.com>
2021-12-29 21:53:58 -0500

    Clean up utilities

--- src/main/kotlin/io/github/amerebagatelle/campbotkotlin/utility/UtilityCommands.kt
+++ src/main/kotlin/io/github/amerebagatelle/campbotkotlin/utility/UtilityCommands.kt
@@ -5,10 +5,7 @@ import dev.kord.core.behavior.channel.createMessage
 import dev.kord.rest.builder.message.create.embed
 import io.github.amerebagatelle.campbotkotlin.Permissions
 import kotlinx.coroutines.delay
-import me.jakejmattson.discordkt.arguments.MessageArg
-import me.jakejmattson.discordkt.arguments.QuoteArg
 import me.jakejmattson.discordkt.commands.commands
-import me.jakejmattson.discordkt.extensions.jumpLink
 import kotlin.io.path.Path
 import kotlin.system.exitProcess
 
@@ -34,29 +31,6 @@ fun ownerUtilityCommands() = commands("Owner", Permissions.BOT_OWNER) {
                 }
                 addFile(Path("./quotes.json"))
             }
-        }
-    }
-}
-
-@Suppress("unused")
-fun userUtilityCommands() = commands("Utility") {
-    globalCommand("reply") {
-        description = "Reply to a specific message, even if it's in another channel."
-        execute(MessageArg(name = "replyTo"), QuoteArg(name = "content")) {
-            message?.delete()
-            channel.createMessage {
-                content = "${args.first.author?.mention} ${getMember()?.displayName} replies: ${args.second}"
-                embed {
-                    title = "Reply To: ${args.first.jumpLink()}"
-                    description = "${args.first.content}"
-                }
-            }
-        }
-    }
-    globalCommand("messageArgTest") {
-        description = "Test message arg"
-        execute(MessageArg) {
-            println(args.first.content)
         }
     }
 }
