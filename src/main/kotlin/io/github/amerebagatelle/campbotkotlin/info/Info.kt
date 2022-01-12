99cbb83d4b8299e94ffbd01cd233bc79ad9d33c5
Brennen Puth <brennenputh@gmail.com>
2022-01-12 08:03:51 -0500

    oop missed one

--- src/main/kotlin/io/github/amerebagatelle/campbotkotlin/info/Info.kt
+++ src/main/kotlin/io/github/amerebagatelle/campbotkotlin/info/Info.kt
@@ -2,6 +2,7 @@ package io.github.amerebagatelle.campbotkotlin.info
 
 import com.beust.klaxon.Klaxon
 import dev.kord.common.entity.Snowflake
+import io.github.amerebagatelle.campbotkotlin.getDataDirectory
 import java.io.FileReader
 import java.io.FileWriter
 
@@ -13,14 +14,15 @@ data class UserInfo(
 )
 
 val klaxon = Klaxon()
+val infoFile = getDataDirectory().resolve("userInfo.json").toFile()
 
 fun getInfo(userId: Snowflake): UserInfo {
-    val infoList = klaxon.parseArray<UserInfo>(FileReader("userInfo.json"))
+    val infoList = klaxon.parseArray<UserInfo>(FileReader(infoFile))
     return infoList?.firstOrNull { it.id == userId.toString() } ?: UserInfo(userId.toString(), "", "", "")
 }
 
 fun updateInfo(userId: Snowflake, info: UserInfo) {
-    val infoList = klaxon.parseArray<UserInfo>(FileReader("userInfo.json"))!!.toMutableList()
+    val infoList = klaxon.parseArray<UserInfo>(FileReader(infoFile))!!.toMutableList()
 
     val entry = infoList.firstOrNull { it.id == userId.toString() }?.let {
         it.username = info.username
@@ -30,7 +32,7 @@ fun updateInfo(userId: Snowflake, info: UserInfo) {
     }
     if (entry == null) infoList.add(info)
 
-    FileWriter("userInfo.json").use {
+    FileWriter(infoFile).use {
         it.write(klaxon.toJsonString(infoList))
         it.close()
     }
