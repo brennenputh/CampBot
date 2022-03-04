59a916639de121d840f45659c8c55f23e82b0a8a
Brennen Puth <brennenputh@gmail.com>
2022-03-04 08:19:43 -0500

    random change

--- src/main/kotlin/io/github/amerebagatelle/campbotkotlin/Main.kt
+++ src/main/kotlin/io/github/amerebagatelle/campbotkotlin/Main.kt
@@ -22,7 +22,7 @@ fun main() {
             permissions = Permissions
         }
         presence {
-            watching("and awake")
+            watching("ya'll")
         }
         onStart {
             loadPictureCache()
