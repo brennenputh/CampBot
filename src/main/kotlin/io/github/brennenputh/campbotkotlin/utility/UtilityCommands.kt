08bd8ad284b140a7ee9fa6333726c6940f76949e
Brennen Puth <brennenputh@gmail.com>
2022-04-08 08:10:53 -0400

    Remove unused shutdown command

--- src/main/kotlin/io/github/brennenputh/campbotkotlin/utility/UtilityCommands.kt
+++ src/main/kotlin/io/github/brennenputh/campbotkotlin/utility/UtilityCommands.kt
@@ -1,29 +1,9 @@
 package io.github.brennenputh.campbotkotlin.utility
 
-import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.behavior.interaction.respondEphemeral
 import dev.kord.rest.builder.message.create.embed
-import io.github.brennenputh.campbotkotlin.EMBED_RED
-import io.github.brennenputh.campbotkotlin.Permissions
 import io.github.brennenputh.campbotkotlin.getDataDirectory
-import kotlinx.coroutines.delay
 import me.jakejmattson.discordkt.commands.commands
-import kotlin.system.exitProcess
-
-@Suppress("unused")
-fun ownerUtilityCommands() = commands("Owner", Permissions.BOT_OWNER) {
-    slash("shutdown") {
-        description = "Shut down the bot."
-        execute {
-            respond {
-                title = "Shutting down in five seconds..."
-                color = EMBED_RED
-            }
-            delay(5000)
-            exitProcess(0)
-        }
-    }
-}
 
 @Suppress("unused")
 fun userUtilityCommands() = commands("Utility") {
