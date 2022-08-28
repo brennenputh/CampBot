ef77beec0ce7261ad3b7a9e7b6ec1cd7fb602e1e
Brennen Puth <brennenputh@gmail.com>
2022-08-27 22:09:28 -0400

    Respond publicly

--- src/main/kotlin/io/github/brennenputh/campbotkotlin/Utility.kt
+++ src/main/kotlin/io/github/brennenputh/campbotkotlin/Utility.kt
@@ -1,6 +1,6 @@
 package io.github.brennenputh.campbotkotlin
 
-import dev.kord.core.behavior.interaction.respondEphemeral
+import dev.kord.core.behavior.interaction.respondPublic
 import dev.kord.rest.builder.message.create.embed
 import me.jakejmattson.discordkt.commands.commands
 
@@ -8,7 +8,7 @@ import me.jakejmattson.discordkt.commands.commands
 fun userUtilityCommands() = commands("Utility") {
     slash("quotesfile", description = "Get the quotes file from the bot.") {
         execute {
-            interaction?.respondEphemeral {
+            interaction?.respondPublic {
                 embed {
                     title = "The quote file for you..."
                 }
